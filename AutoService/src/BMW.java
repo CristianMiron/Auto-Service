075a96c3e2a7da9f7718259e5901f402b006d804
Cristian Miron <Cristian Miron@DESKTOP-T9R9075.mshome.net>
2019-09-23 16:28:05 +0300

    Added BMW Class

---
+++ AutoService/src/BMW.java
@@ -0,0 +1,18 @@
+
+class BMW extends Car{
+
+	private boolean quattro;
+	
+	public BMW(String brand, int fabricationYear, String color, boolean quattro) {
+		super(brand, fabricationYear, color);
+		this.quattro = quattro;
+	}
+	
+	public String toString() {
+		String result = (quattro == true) ? "the car is quattro" : "the car is not quattro";
+
+		return super.toStrin() + result;
+
+	}
+	
+}
