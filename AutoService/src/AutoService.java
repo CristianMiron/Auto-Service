7eacaf3ab1222571136d0e7703ed895f1e917dc2
Cristian Miron <Cristian Miron@DESKTOP-T9R9075.mshome.net>
2019-09-23 16:48:44 +0300

    Modified toString methods

--- AutoService/src/AutoService.java
+++ AutoService/src/AutoService.java
@@ -1,4 +1,32 @@
+import java.util.*;
 
-public class AutoService {
+class AutoService {
+
+	private String name;
+	private int serviceCapacity;
+	private ArrayList<Car> carsInService;
+
+	public AutoService(String name, int serviceCapacity) {
+		this.name = name;
+		this.serviceCapacity = serviceCapacity;
+		this.carsInService = new ArrayList<Car>(serviceCapacity);
+	}
+
+	public boolean addCarInService(Car c) throws ServiceIsFullException {
+		if (carsInService.size() > serviceCapacity) {
+			throw new ServiceIsFullException("The service is full at this moment");
+		} else {
+			return carsInService.add(c);
+		}
+	}
+
+	public String toString() {
+		String result = "Cars in Service: \n";
+
+		for (Car x : carsInService) {
+			result += x.toString();
+		}
+		return result;
+	}
 
 }
